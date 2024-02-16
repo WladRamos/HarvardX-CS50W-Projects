87ee33b133d571bfe52c7d13eeec6bc4ec4f3daf
WladRamos <wladimirramos88@gmail.com>
2024-02-16 20:01:40 -0300

    Inicio mail

--- Projects/mail/mail/static/mail/inbox.js
+++ Projects/mail/mail/static/mail/inbox.js
@@ -20,6 +20,37 @@ function compose_email() {
   document.querySelector('#compose-recipients').value = '';
   document.querySelector('#compose-subject').value = '';
   document.querySelector('#compose-body').value = '';
+
+  //call function to send email when form is submitted
+  document.querySelector('#compose-form').onsubmit = function(){
+    const email_recipient = document.querySelector('#compose-recipients').value;
+    const email_subject = document.querySelector('#compose-subject').value;
+    const email_body = document.querySelector('#compose-body').value;
+    send_email(email_recipient, email_subject, email_body);
+  }
+}
+
+function send_email(email_recipient, email_subject, email_body){
+  
+  fetch('/emails', {
+    method: 'POST',
+    body: JSON.stringify({
+        recipients: email_recipient,
+        subject: email_subject,
+        body: email_body
+    })
+  })
+  .then(response => response.json())
+  .then(result => {
+    // Print result
+    console.log(result);
+    //load the user’s sent mailbox
+    load_mailbox('sent')
+  })
+  .catch(error =>{
+    console.log('Error:', error)
+  });
+  return false;
 }
 
 function load_mailbox(mailbox) {
@@ -30,4 +61,17 @@ function load_mailbox(mailbox) {
 
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
+
+  //load the appropriate mailbox
+  fetch(`/emails/${mailbox}`)
+  .then(response => response.json())
+  .then(emails => {
+    // Print emails
+    console.log(emails);
+
+    // ... do something else with emails ...
+  })
+  .catch(error =>{
+    console.log('Error:', error)
+  });
 }
